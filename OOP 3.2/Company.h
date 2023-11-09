d00c0c569dfe58d3fe2057a46f1c30d6a07e5fff
LastWinter <neretin-205@bk.ru>
2023-11-09 12:48:46 +0300

    task is done

--- OOP 3.2/Company.h
+++ OOP 3.2/Company.h
@@ -6,4 +6,22 @@ public:
     std::string name; // название компании
     int persons; // количество сотрудников
     int money; // месячный фонд зарплаты
+
+    void show() {
+        std::cout << "В компании " << name << " трудятся " << persons << " сотрудников " << std::endl;
+        std::cout << "Фонд зарплаты: " << money << std::endl;
+    }
+
+    int averageSalary() {
+        return money / persons;
+    }
+
+    int maxPersons(int salary) {
+        return money / salary;
+    }
+
+    int minus(int minus1, int minus2) {
+     
+        return money * (minus1 + minus2) / 100;
+    }
 };
---
+++ OOP 3.2/OOP3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <string>
+#include "Company.h"
+#include "Text.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+    Company company1;
+    company1.name = "Computers";
+    company1.persons = 100;
+    company1.money = 1000000;
+
+    Company company2;
+    company2.name = "Books";
+    company2.persons = 200;
+    company2.money = 2000000;
+
+    // появление недоступного объекта
+    company1 = company2;
+
+    cout << "В компании " << company1.name << " трудятся " << company1.persons << " сотрудников" << endl;
+    cout << "Фонд зарплаты: " << company1.money << endl;
+    Text::displayText();
+
+    //создание и инициализация объекта
+    Company factory;
+    factory.name = "Премьер";
+    factory.persons = 100;
+    factory.money = 1000000;
+
+    //вызовы методов
+    factory.show();
+    cout << "Средняя зарплата: " << factory.averageSalary() << endl;
+    cout << "Отчисления: " << factory.minus(13, 4) << endl;
+
+    cout << "Средняя зарплата?" << endl;
+    int average;
+    cin >> average;
+    cout << "Количество сотрудников: " << factory.maxPersons(average) << endl;
+
+
+	return 0;
+}
+
+
+
