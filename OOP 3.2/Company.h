0aa05a1d9f90759c170c4f1935e1b8b37420dc0d
LastWinter <neretin-205@bk.ru>
2023-11-09 10:30:55 +0300

    OOP 3.2Добавить .gitattributes и .gitignore.

---
+++ OOP 3.2/Company.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include <string>
+class Company {
+public:
+    std::string name; // название компании
+    int persons; // количество сотрудников
+    int money; // мес€чный фонд зарплаты
+};
---
+++ OOP 3.2/shablon.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+#include "Company.h"
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "RU");
+    Company company1;
+    company1.name = "Computers";
+    company1.persons = 100;
+    company1.money = 1000000; // рис. 3.4а
+
+    Company company2;
+    company2.name = "Books";
+    company2.persons = 200;
+    company2.money = 2000000; // рис. 3.4б
+
+    // появление недоступного объекта
+    company1 = company2; // рис.3.4с
+
+    cout << "В компании " << company1.name << " трудятся " << company1.persons << " сотрудников" << endl;
+    cout << "Фонд зарплаты: " << company1.money << endl;
+
+	return 0;
+}
+
+
+
